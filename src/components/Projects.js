f1429aaf62db4a6c5f6312517ddf7b4cec381c8b
Seif Ashraf <seif_fire2@hotmail.com>
2023-01-03 17:24:08 +0200

    updated data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => {
         {
             id:1,
             title: 'MyReads',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur cumque aperiam in perferendis! In, nesciunt?',
+            description: 'Ebook app where you can manage your favourite books by searching for them and adding them to their specified shelves',
             tags: ['Ebooks', 'Responsive', 'ReactJs', 'API server', 'JavaScript'],
             thumbnail: process.env.PUBLIC_URL + '/Assets/Images/myreads.png',
             link: 'https://myreads-seifelkady.netlify.app'
@@ -15,16 +15,16 @@ const Projects = () => {
         {
             id:2,
             title: 'E-Commerce Page UI',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur cumque aperiam in perferendis! In, nesciunt?',
-            tags: ['Branding', 'E-Commerce', 'Responsive', 'JavaScript'],
+            description: 'Responsive product page using React, HTML and CSS',
+            tags: ['Branding', 'E-Commerce', 'React', 'Responsive', 'JavaScript'],
             thumbnail: process.env.PUBLIC_URL + '/Assets/Images/e-commerce-page.png',
             link: 'https://e-commerce-page-seifelkady.netlify.app/'
         },
         {
             id:3,
             title: 'MERN Stack Restaurant website',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur cumque aperiam in perferendis! In, nesciunt?',
-            tags: ['Branding', 'CRUD', 'Responsive'],
+            description: 'Landing page for a seafood restaurant. User can preform CRUD operations on this app to manipulate menu items that are saved to a dedicated database.',
+            tags: ['Branding', 'CRUD', 'Responsive', 'MERN'],
             thumbnail: process.env.PUBLIC_URL + '/Assets/Images/seafood.png',
             link: 'https://github.com/Sayyfz/seafood-site'
         },
