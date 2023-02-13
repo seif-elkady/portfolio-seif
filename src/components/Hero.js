2ee58e195b47d493318ffd434ce8037487e33811
Seif Ashraf <seif_fire2@hotmail.com>
2023-02-13 11:40:42 +0200

    updated date

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
             />
             <div className='container'>
                 <h1 className='hero__title text-center animated-sections'>
-                    Design your landing page from scratch, help your brand stay mighty{' '}
+                    Design your Web App from scratch, help your brand stay mighty{' '}
                 </h1>
                 <p className='hero__text text-center animated-sections'>
                     I am a software developer with an affinity for working with creative clients
